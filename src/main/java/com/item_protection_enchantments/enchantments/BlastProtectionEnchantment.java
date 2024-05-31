9b137b0f0f883bae5e4e63c93263e72046211ba4
Torikun9971 <msn20220326@gmail.com>
2024-06-01 01:58:02 +0900

    🔧 items that are by default fireproof can no longer receive the fire protection enchantment 🔧 nether star can no longer receive blast protection enchantment 🔄️ code cleanup

--- src/main/java/com/item_protection_enchantments/enchantments/BlastProtectionEnchantment.java
+++ src/main/java/com/item_protection_enchantments/enchantments/BlastProtectionEnchantment.java
@@ -1,6 +1,8 @@
 package com.item_protection_enchantments.enchantments;
 
 import com.item_protection_enchantments.config.ModConfiguration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 public class BlastProtectionEnchantment extends ItemProtectionEnchantment {
     @Override
@@ -11,5 +13,14 @@ public class BlastProtectionEnchantment extends ItemProtectionEnchantment {
     @Override
     public Rarity getRarity() {
         return ModConfiguration.getConfig().blastProtectionEnchantment.rarity;
+    }
+
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        if (super.isAcceptableItem(stack)) {
+            return stack.getItem() != Items.NETHER_STAR;
+        }
+
+        return false;
     }
 }
--- src/main/java/com/item_protection_enchantments/enchantments/FireProtectionEnchantment.java
+++ src/main/java/com/item_protection_enchantments/enchantments/FireProtectionEnchantment.java
@@ -1,6 +1,7 @@
 package com.item_protection_enchantments.enchantments;
 
 import com.item_protection_enchantments.config.ModConfiguration;
+import net.minecraft.item.ItemStack;
 
 public class FireProtectionEnchantment extends ItemProtectionEnchantment {
     @Override
@@ -11,5 +12,14 @@ public class FireProtectionEnchantment extends ItemProtectionEnchantment {
     @Override
     public Rarity getRarity() {
         return ModConfiguration.getConfig().fireProtectionEnchantment.rarity;
+    }
+
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        if (super.isAcceptableItem(stack)) {
+            return !stack.getItem().isFireproof();
+        }
+
+        return false;
     }
 }
