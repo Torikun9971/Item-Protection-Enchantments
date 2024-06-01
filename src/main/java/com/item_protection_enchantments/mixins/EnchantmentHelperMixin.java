b74367ba48ac8d5b282853362312f02c8103627f
Torikun9971 <msn20220326@gmail.com>
2024-06-01 23:52:45 +0900

    🔧 fixed an issue where enchantments that should not be applicable could be applied through enchantment table

---
+++ src/main/java/com/item_protection_enchantments/mixins/EnchantmentHelperMixin.java
@@ -0,0 +1,31 @@
+package com.item_protection_enchantments.mixins;
+
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Random;
+
+@Mixin(EnchantmentHelper.class)
+public abstract class EnchantmentHelperMixin {
+    @Inject(method = "selectEnchantment", at = @At("RETURN"), cancellable = true)
+    private static void protection_enchantments$selectEnchantment(Random random, ItemStack stack, int level, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentData>> cir) {
+        List<EnchantmentData> enchantments = cir.getReturnValue();
+        boolean hasInvalidEnchantment = true;
+
+        while (hasInvalidEnchantment) {
+            hasInvalidEnchantment = enchantments.stream().anyMatch((entry) -> !entry.enchantment.canEnchant(stack));
+
+            if (hasInvalidEnchantment) {
+                enchantments = EnchantmentHelper.selectEnchantment(random, stack, level, treasureAllowed);
+            }
+        }
+
+        cir.setReturnValue(enchantments);
+    }
+}
