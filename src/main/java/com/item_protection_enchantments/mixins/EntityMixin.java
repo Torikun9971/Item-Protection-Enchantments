a91a64a240b71f864745daa47984f7583671eb96
Torikun9971 <msn20220326@gmail.com>
2024-06-17 23:07:13 +0900

    📣 Add Inventory Holding enchantment (default as treasure enchantment) 📣 Change config enchantableItems to individual settings 📣 Add ITEMS_ONLY to the value of enchantableItems in config 📣 Make treasure enchantment configurable in config 📣 Make availability through villager trading configurable in config 📣 Make Void Protection's protection height configurable in config 📣 Make Void Protection's post-protection height configurable in config 📣 Make disabling Curse of Vanishing with Inventory Holding configurable in config 📣 Make fixing the bug where anvils falling on items would cause the items to disappear configurable in config 📖 Update translations ⬆️ Update mod version 🔧 mod adjustments 🔄️ code cleanup

--- src/main/java/com/item_protection_enchantments/mixins/EntityMixin.java
+++ src/main/java/com/item_protection_enchantments/mixins/EntityMixin.java
@@ -3,36 +3,49 @@ package com.item_protection_enchantments.mixins;
 import com.item_protection_enchantments.ItemProtectionEnchantments;
 import com.item_protection_enchantments.config.ModConfiguration;
 import com.item_protection_enchantments.init.ModEnchantments;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-    @Inject(method = "lavaHurt", at = @At("HEAD"), cancellable = true)
-    public void protection_enchantments$lavaHurt(CallbackInfo ci) {
-        if ((Object) this instanceof ItemEntity itemEntity) {
-            if (ItemProtectionEnchantments.hasEnchantment(itemEntity.getItem(), true, ModEnchantments.FIRE_PROTECTION_ITEM.get())) {
-                if (!itemEntity.hasGlowingTag()) itemEntity.setGlowingTag(ModConfiguration.FIRE_PROTECTION_GLOWING_TAG.get());
-                ci.cancel();
+    @Inject(method = "checkOutOfWorld", at = @At("HEAD"), cancellable = true)
+    public void protection_enchantments$checkOutOfWorld(CallbackInfo ci) {
+        switch (ModConfiguration.VOID_PROTECTION_PROTECTION_HEIGHT.get()) {
+            case MIN_BUILD_HEIGHT -> {
+                if ((Object) this instanceof ItemEntity itemEntity) {
+                    if (itemEntity.getY() < itemEntity.level.getMinBuildHeight()) {
+                        protection_enchantments$protection();
+                        ci.cancel();
+                    }
+                }
             }
         }
     }
 
     @Inject(method = "outOfWorld", at = @At("HEAD"), cancellable = true)
     protected void protection_enchantments$outOfWorld(CallbackInfo ci) {
+        switch (ModConfiguration.VOID_PROTECTION_PROTECTION_HEIGHT.get()) {
+            case HEIGHT_WHERE_ENTITY_TAKES_DAMAGE -> {
+                protection_enchantments$protection();
+                ci.cancel();
+            }
+        }
+    }
+
+    @Unique
+    private void protection_enchantments$protection() {
         if ((Object) this instanceof ItemEntity itemEntity) {
             if (ItemProtectionEnchantments.hasEnchantment(itemEntity.getItem(), true, ModEnchantments.VOID_PROTECTION_ITEM.get())) {
                 itemEntity.setNoGravity(true);
                 itemEntity.setInvulnerable(true);
-                itemEntity.setPos(itemEntity.getX(), itemEntity.getLevel().getMinBuildHeight() + 1, itemEntity.getZ());
+                itemEntity.setPos(itemEntity.getX(), itemEntity.level.getMinBuildHeight() + ModConfiguration.VOID_PROTECTION_PROTECTED_HEIGHT.get(), itemEntity.getZ());
                 itemEntity.setDeltaMovement(0, 0, 0);
                 if (!itemEntity.hasGlowingTag()) itemEntity.setGlowingTag(ModConfiguration.VOID_PROTECTION_GLOWING_TAG.get());
-
-                ci.cancel();
             }
         }
     }
