2c5825335a831bb64859abac8ae3ecd106fcb038
Torikun9971 <msn20220326@gmail.com>
2024-06-19 21:37:36 +0900

    📣 Add Inventory Holding enchantment (default as treasure enchantment) 📣 Change config enchantableItems to individual settings 📣 Add ITEMS_ONLY to the value of enchantableItems in config 📣 Make treasure enchantment configurable in config 📣 Make availability through villager trading configurable in config 📣 Make Void Protection's protection height configurable in config 📣 Make Void Protection's post-protection height configurable in config 📣 Make disabling Curse of Vanishing with Inventory Holding configurable in config 📖 Update translations ⬆️ Update mod version 🔧 mod adjustments 🔄️ code cleanup

--- src/main/java/com/item_protection_enchantments/mixins/ShulkerBoxBlockMixin.java
+++ src/main/java/com/item_protection_enchantments/mixins/ShulkerBoxBlockMixin.java
@@ -2,62 +2,33 @@ package com.item_protection_enchantments.mixins;
 
 import com.item_protection_enchantments.blockentities.EnchantableBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
 
 @Mixin(ShulkerBoxBlock.class)
-public abstract class ShulkerBoxBlockMixin extends BaseEntityBlock {
-    @Shadow
-    @Nullable
-    public abstract DyeColor getColor();
+public abstract class ShulkerBoxBlockMixin {
+    @Redirect(method = "playerWillDestroy", at = @At(value = "NEW", target = "(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;", ordinal = 0))
+    private ItemEntity protection_enchantments$playerWillDestroy(Level level, double x, double y, double z, ItemStack stack) {
+        BlockEntity blockEntity = level.getBlockEntity(new BlockPos(Mth.floor(x),Mth.floor(y),Mth.floor(z)));
 
-    public ShulkerBoxBlockMixin(Properties properties) {
-        super(properties);
-    }
-
-    @Inject(method = "playerWillDestroy", at = @At("HEAD"), cancellable = true)
-    public void protection_enchantments$playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player, CallbackInfoReturnable<BlockState> cir) {
-        BlockEntity blockentity = level.getBlockEntity(pos);
-        if (blockentity instanceof ShulkerBoxBlockEntity shulkerboxblockentity) {
-            if (!level.isClientSide && player.isCreative() && !shulkerboxblockentity.isEmpty()) {
-                ItemStack itemstack = ShulkerBoxBlock.getColoredItemStack(this.getColor());
-                blockentity.saveToItem(itemstack);
-                if (shulkerboxblockentity.hasCustomName()) {
-                    itemstack.setHoverName(shulkerboxblockentity.getCustomName());
-                }
-
-                if (blockentity instanceof EnchantableBlock enchantableBlock) {
-                    if (enchantableBlock.getEnchantmentTag() != null) {
-                        itemstack.getOrCreateTag().put("Enchantments", enchantableBlock.getEnchantmentTag());
-                    }
-                }
-
-                ItemEntity itementity = new ItemEntity(level, (double)pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5, itemstack);
-                itementity.setDefaultPickUpDelay();
-                level.addFreshEntity(itementity);
-            } else {
-                shulkerboxblockentity.unpackLootTable(player);
+        if (blockEntity instanceof EnchantableBlock enchantableBlock) {
+            if (enchantableBlock.getEnchantmentTag() != null) {
+                stack.getOrCreateTag().put("Enchantments", enchantableBlock.getEnchantmentTag());
             }
         }
 
-        cir.setReturnValue(super.playerWillDestroy(level, pos, state, player));
+        return new ItemEntity(level, x, y, z, stack);
     }
 
     @Inject(method = "setPlacedBy", at = @At("HEAD"))
