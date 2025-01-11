91f65cf5b8ce489d836ffeb40f2e4c61e51654bd
Torikun9971 <msn20220326@gmail.com>
2025-01-11 17:39:20 +0900

    🔧 Fix mods.toml format 🔧 Fix crash when placing books on enchantment table 🔧 Correct modrinth projectId 🔧 Change step order in release.yml

--- src/main/java/com/torikun9971/itemprotectionenchantments/enchantments/BaseProtectionEnchantment.java
+++ src/main/java/com/torikun9971/itemprotectionenchantments/enchantments/BaseProtectionEnchantment.java
@@ -51,6 +51,11 @@ public abstract class BaseProtectionEnchantment extends Enchantment {
         return getConfig().isTradeable();
     }
 
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack) {
+        return canEnchant(stack);
+    }
+
     protected abstract IBaseProtectionConfig getConfig();
 
     public enum EnchantmentTypes {
